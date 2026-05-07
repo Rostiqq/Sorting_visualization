5547e7bd9277ab3ced2e73ee3ea347412d3e141e
Rastislav <rastislavmacko99@gmail.com>
2026-05-07 20:40:02 +0200

    add: generate array and draw it

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+#include <random>
+
+void generateArray(std::vector<int> &array)
+{
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "TEST");
+    int windowHeight = 600;
+    int windowWidth = 1000;
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Sorting");
+    std::vector<int> hodnoty;
+    hodnoty.reserve(100);
 
-    sf::CircleShape shape(50);
-    shape.setFillColor(sf::Color::Red);
+    std::random_device random;
+    std::mt19937 gen(random());
+    std::uniform_int_distribution<int> distInt(1, 100);
+
+    for (int i = 0; i < 100; i++)
+    {
+        int value = distInt(gen);
+        hodnoty.push_back(value);
+        std::cout << value << std::endl;
+    }
+    int widthColumn = windowWidth / 100;
+    int scale = windowHeight / 100;
+    
 
     while (window.isOpen())
     {
-        std::cout << "Start\n";
-
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -20,7 +39,18 @@ int main()
         }
 
         window.clear(sf::Color::Black);
-        window.draw(shape);   
+        for (int i = 0; i < hodnoty.size(); i++)
+        {
+            int heightColumn = hodnoty[i] * scale;
+            int x = widthColumn * i;
+            int y = windowHeight - heightColumn;
+            
+            sf::RectangleShape rec;
+            rec.setSize(sf::Vector2f(widthColumn, heightColumn));
+            rec.setPosition(sf::Vector2f(x, y));
+            rec.setFillColor(sf::Color::Blue);
+            window.draw(rec);
+        }
         window.display();
     }
 }
