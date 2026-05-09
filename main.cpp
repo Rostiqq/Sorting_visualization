458f9e818d1796c1b7e2fc804e61bb3619d4cb6d
Rastislav <rastislavmacko99@gmail.com>
2026-05-09 20:06:21 +0200

    bubble sort by step

--- main.cpp
+++ main.cpp
@@ -7,10 +7,15 @@ int main()
 {
     int windowHeight = 600;
     int windowWidth = 1000;
+    bool sorting = true;
+    bool sorted = false;
+    int i = 0;
+    int j = 0;
+    int temp;
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Sorting");
     int amount;
-    std::cout<<"Enter amount of rectangle: ";
-    std::cin>>amount;
+    std::cout << "Enter amount of rectangle: ";
+    std::cin >> amount;
     std::vector<int> hodnoty;
     int maxValue = amount;
     std::random_device random;
@@ -24,8 +29,7 @@ int main()
         std::cout << value << std::endl;
     }
     float widthColumn = static_cast<float>(windowWidth) / amount;
-    float scale = static_cast<float>(windowHeight)/ amount;
-    
+    float scale = static_cast<float>(windowHeight) / amount;
 
     while (window.isOpen())
     {
@@ -42,13 +46,37 @@ int main()
             float heightColumn = hodnoty[i] * scale;
             float x = widthColumn * i;
             float y = windowHeight - heightColumn;
-            
+
             sf::RectangleShape rec;
             rec.setSize(sf::Vector2f(widthColumn, heightColumn));
             rec.setPosition(sf::Vector2f(x, y));
             rec.setFillColor(sf::Color::Blue);
             window.draw(rec);
         }
+
+        if (sorting)
+        {
+            if (j + 1 >= amount)
+                return 0;
+            if (hodnoty[j] > hodnoty[j + 1])
+            {
+                temp = hodnoty[j];
+                hodnoty[j] = hodnoty[j + 1];
+                hodnoty[j + 1] = temp;
+            }
+            j++;
+            if (j >= amount - i - 1)
+            {
+                j = 0;
+                i++;
+            }
+            if (i >= amount - 1)
+            {
+                sorting = false;
+                sorted = true;
+            }
+        }
+
         window.display();
     }
 }
